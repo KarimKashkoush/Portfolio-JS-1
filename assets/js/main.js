f3bfb571e98a96c1b91d1b78fb299222661b97d1
KarimKashkoush <karimkashkoush5@gmail.com>
2023-12-11 20:26:35 +0200

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,20 @@
 let header = document.getElementById("header");
 document.onscroll = () => {
     if (scrollY > 250) {
-        header.style.backgroundColor = "#090516";
+        header.style.backgroundColor = "#1a1330";
         header.style.boxShadow = "0 -5px 20px -5px white";
+        header.style.paddingBlock = "10px";
     } else {
         header.style.backgroundColor = "transparent";
         header.style.boxShadow = "none";
+        header.style.paddingBlock = "20px";
     }
 };
+if (scrollY > 250) {
+    header.style.backgroundColor = "#1a1330";
+    header.style.boxShadow = "0 -5px 20px -5px white";
+    header.style.paddingBlock = "10px";
+}
 // =============== //
 let openNav = document.getElementById("openNav");
 let CloseNav = document.getElementById("CloseNav");
@@ -29,3 +36,20 @@ navLinks.forEach((ele)=> {
         this.classList.add("active")
     }
 })
+
+// =============== //
+let readMe = document.getElementById("readMe");
+let openReadMe = document.getElementById("openReadMe");
+let closeReadMe = document.getElementById("closeReadMe");
+
+openReadMe.onclick = function() {
+    readMe.style.display = 'block';
+}
+closeReadMe.onclick = function() {
+    readMe.style.display = 'none';
+}
+// =============== //
+
+let years = document.querySelector("#copyRight span");
+
+years.innerHTML = new Date().getFullYear()
